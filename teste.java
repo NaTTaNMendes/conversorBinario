8ca2f491f25f767c11cbaf8a9d30946d986a62b6
Nattan <nattan078@gmail.com>
2022-03-29 13:31:06 -0300

    Mais melhorias

--- teste.java
+++ teste.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.math.*;
 
 public class teste {
     public static void main(String[] args) {
@@ -12,22 +9,26 @@ public class teste {
         int choice = sc.nextInt();
         System.out.println();
 
-        String number = "";
+        int number = 0;
         String binary = "";
 
         switch(choice) {
             case 1: 
                 System.out.print("Positive integer: ");
-                number = sc.next();
+                number = sc.nextInt();
                 binary = unsigned(number);
                 break;
             case 2:
                 System.out.print("Integer: ");
-                number = sc.next();
+                number = sc.nextInt();
                 binary = signAndMagnitude(number);
                 break;
+            case 3:
+                System.out.print("Integer: ");
+                number = sc.nextInt();
+                binary = twoComplement(number);
+                break;
             default:
-                binary = "Not implemented yet";
                 break;
         }
         
@@ -35,38 +36,53 @@ public class teste {
         sc.close();        
     }
 
-    public static String unsigned(String number){
-        int value = Integer.parseInt(number);
+    public static String unsigned(Integer number){
         int r = 0;
         String st = "";
-        while(value != 0){
-            r = value % 2;
+        while(number != 0){
+            r = number % 2;
             st = r + st;
-            value = value/2;
+            number = number / 2;
         }
         return st;
     }
 
-    public static String signAndMagnitude(String number){
+    public static String signAndMagnitude(Integer number){
         String st = "";
         String sign = "";
-        if (number.charAt(0) == '-'){
+        if (number < 0){
             sign = "1";
-            st = unsigned(number.substring(1));
+            st = unsigned(number * -1);
         } else {
             sign = "0";
-            st = unsigned(number.substring(0));
+            st = unsigned(number);
         }
         return sign + st;
     }
 
-    /*public static String twoComplement(String number){
-        char sign = number.charAt(0);
-        if (sign == '+'){
-            return unsigned(number.substring(1));
+    public static String twoComplement(Integer number){
+        String st = "";
+        String begin = "";
+        String end = "";
+        if (number < 0){
+            st = unsigned(number * -1);
+            for(int i = st.length() - 1; i >= 0; i--){
+                if (st.charAt(i) == '1'){
+                    end = st.substring(i, st.length());
+                    break;
+                }
+            }
+            for(int i = st.length() - 1; i >= 0; i--){
+                if (st.charAt(i) == '1'){
+                    begin = begin + "0";
+                } else {
+                    begin = begin + "1";
+                }
+            }
+            return begin + end;
         } else {
-            sign = '1';
-        }
-        int value = Integer.parseInt(number.substring(1));
-    }*/
+            st = unsigned(number);
+            return st;
+        }        
+    }
 }
